9e6605ba55e8242a13ce9164583b32a2764f465f
fuzzfizz <athit0900@gmail.com>
2026-07-31 14:56:46 +0700

    feat: add useCustomGeminiKeyProvider for settings toggle state

--- lib/core/providers/gemini_api_key_provider.dart
+++ lib/core/providers/gemini_api_key_provider.dart
@@ -32,3 +32,29 @@ class GeminiApiKeyNotifier extends StateNotifier<String?> {
 final geminiApiKeyProvider = StateNotifierProvider<GeminiApiKeyNotifier, String?>((ref) {
   return GeminiApiKeyNotifier();
 });
+
+class UseCustomGeminiKeyNotifier extends StateNotifier<bool> {
+  static const _key = 'use_custom_gemini_api_key';
+
+  UseCustomGeminiKeyNotifier() : super(false) {
+    _init();
+  }
+
+  Future<void> _init() async {
+    final prefs = await SharedPreferences.getInstance();
+    final value = prefs.getBool(_key);
+    if (value != null && mounted) {
+      state = value;
+    }
+  }
+
+  Future<void> setUseCustomKey(bool value) async {
+    state = value;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_key, value);
+  }
+}
+
+final useCustomGeminiKeyProvider = StateNotifierProvider<UseCustomGeminiKeyNotifier, bool>((ref) {
+  return UseCustomGeminiKeyNotifier();
+});
