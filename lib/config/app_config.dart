a700eb52768c6114237a2e6ae0ef45099256a1d8
fuzzfizz <athit0900@gmail.com>
2026-09-02 15:23:53 +0700

    chore: remove unused dependencies and enable R8 resource shrinking

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -3,6 +3,4 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 class AppConfig {
   static String get supabaseUrl => dotenv.env['SUPABASE_URL'] ?? '';
   static String get supabaseAnonKey => dotenv.env['SUPABASE_ANON_KEY'] ?? '';
-  static String get geminiApiKey => dotenv.env['GEMINI_API_KEY'] ?? '';
-  static String get deepseekApiKey => dotenv.env['DEEPSEEK_API_KEY'] ?? '';
 }
